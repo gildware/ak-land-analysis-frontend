151b6d8760dd4081c5a84af0b316d5f9f29ef6a7
Kamran <kamran@gildware.com>
2026-01-07 13:22:09 +0530

    base

--- src/map/controls/navigation.ts
+++
@@ -1,5 +0,0 @@
-import mapboxgl, { Map } from "mapbox-gl";
-
-export function addNavigation(map: Map) {
-  map.addControl(new mapboxgl.NavigationControl(), "top-right");
-}
--- src/map/layers/indexRegistry.ts
+++
@@ -1,7 +0,0 @@
-import { toggleNDVI } from "./ndviLayer";
-
-export const INDEX_HANDLERS = {
-  NDVI: toggleNDVI,
-  // EVI: toggleEVI,
-  // NDWI: toggleNDWI,
-};
